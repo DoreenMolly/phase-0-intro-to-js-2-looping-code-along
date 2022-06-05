2584daaee0cd70520b7924e1f82a063966534f69
Doreen <wanjirudoreen13@gmail.com>
2022-06-05 17:54:41 +0300

    commit tree 10f8ec803d75d0af8a3e052ba88e87106dd8f50d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names,event) {
+    let thankYoucards = [];
+    for (let i = 0; i < names.length; i++){
+        thankYoucards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYoucards;
+}
+function countDown (number){
+    while (number > 0) {
+        console.log(number);
+        number -=1;
+    }
+    console.log(number);
+}
